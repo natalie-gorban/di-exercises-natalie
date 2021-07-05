9569e60c28904d210e2d83afa538af0a82aae969
Natalie Gorban <natalie.v.gorban@gmail.com>
2021-07-05 09:34:11 +0300

    home exercise

---
+++ day6/script.js
@@ -0,0 +1,59 @@
+// Exercise 1
+
+let addressNumder = 5;
+let addressStreet = 'BenYehuda';
+let country = 'Israel';
+
+// Exercise 2
+let globalAddress = `I live ${addressStreet} ${addressNumder} in ${country }`
+console.log(globalAddress);
+
+let myBirthYear = 1983
+let futureYear = 2022
+let futureAge = futureYear - myBirthYear
+let possibleAge = `I will be ${futureAge} in ${futureYear}`
+console.log(possibleAge);
+
+// Exercise 3
+let pets = ['cat', 'dog', 'fish', 'rabbit', 'cow']
+console.log(pets);
+
+let last = pets[pets.length - 4]
+console.log(last)
+
+pets.push('horse')
+console.log(pets)
+
+let removedItem = pets.splice(3)
+console.log(pets)
+
+
+console.log(pets.length)
+
+
+//Exercise XP
+console.log(5 + '34')
+
+console.log(5-'4')
+
+console.log(10%5)
+
+console.log(5%10)
+
+console.log('java' + 'Script')
+
+console.log(' ' + ' ')
+
+console.log(' ' + 0)
+
+console.log(true + true)
+
+console.log(true + false)
+
+console.log(false + true)
+
+console.log(false - true)
+
+console.log(3 - 4)
+
+console.log('Bob' - 'bill')
