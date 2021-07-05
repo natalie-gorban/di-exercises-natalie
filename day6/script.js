e75afcca0d1f356143d4ee490cf97225d0bae4ad
Natalie Gorban <natalie.v.gorban@gmail.com>
2021-07-05 14:25:29 +0300

    additions

--- day6/script.js
+++ day6/script.js
@@ -57,3 +57,33 @@ console.log(false - true)
 console.log(3 - 4)
 
 console.log('Bob' - 'bill')
+
+
+// Exercise 1: Daily Challenge
+
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift();
+console.log(fruits);
+
+fruits.sort();
+console.log(fruits);
+
+fruits.push('Kiwi');
+console.log(fruits);
+
+fruits.splice(0,1);
+console.log(fruits);
+
+fruits.reverse();
+console.log(fruits);
+
+
+
+
+// Exercise 2:
+let moreFruits = ['Banana', ['Apples', ['Oranges'], 'Blueberries']]
+
+console.log(moreFruits[1][1][0]);
+
