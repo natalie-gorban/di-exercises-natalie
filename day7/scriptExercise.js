8da4725e8d89d870bd1cc05627f6a7968b9fb6df
Natalie Gorban <natalie.v.gorban@gmail.com>
2021-07-05 23:30:04 +0300

    day7 + portfolio draft

---
+++ day7/scriptExercise.js
@@ -0,0 +1,71 @@
+// Exercise 1
+let x = 5;
+let y = 2;
+
+if(x >= y){
+    console.log('x is the biggest number')
+} else {
+    console.log('x is not the biggest number')
+}
+
+
+// Exercise 2: Chihuahua
+
+let newDog = 'Chihuahua';
+console.log(newDog.length);
+
+console.log('newDog'.toUpperCase())
+console.log('newDog'.toLowerCase())
+
+if (newDog === 'Chihuahua'){
+    console.log('I love Chihuahuas, it`s my favorite dog breed')
+} else {
+    console.log('I don`t care, I prefer cats')
+}
+
+
+// Exercise 3:
+let x = 'user number'
+x = 254
+
+if ( x % 2 == 0){
+  console.log('x is an even number')
+} else (x % 2 !== 0)
+ console.log(' x is an odd number')
+
+
+
+
+// Exercise 4:
+
+let users = ['Lea123', 'Princess45', 'cat&doglovers', 'helooo@ooo']
+
+console.log(users.length)
+
+let name_user1 = 'Lea123'
+let name_user2 = 'Princess45'
+let online = 3
+
+
+if (online === 0) {
+    console.log('no one is online')
+} else if (online === 1) {
+    console.log(name_user1 + ' is online')
+} else if (online === 2) {
+    console.log(name_user1, name_user2 + ' are online')
+} else {
+    console.log(name_user1, name_user2 + ' and 3 more are online')
+}
+
+
+
+
+
+// Daily Challenge: Not Bad
+
+  let sentence = 'The movie is not that bad, I like it'
+
+
+  let replaced = sentence.replace(/\bnot\b.*\bbad\b/, "good")
+
+  console.log(replaced)
